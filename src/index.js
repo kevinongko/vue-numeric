463b3523fad22efe7c50acb2338b1500294faf68
Kevin <kevin.ongko@gmail.com>
2017-08-06 00:58:37 +0700

    remove extra export

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import component from './vue-numeric.vue'
 
 const plugin = {
   install: Vue => {
-    Vue.component('vue-numeric', component)
+    Vue.component(component.name, component)
   }
 };
 
 component.install = plugin.install
 
 export default component
-export { component, plugin }
